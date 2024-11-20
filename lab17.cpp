9fb59660e8af7dc38490dd4476c75b8fa235a8d7
Emma Simpson <simpemma@umich.edu>
2024-11-20 10:12:18 -0500

    first attempt

--- lab17.cpp
+++ lab17.cpp
@@ -1,1 +1,14 @@
 // Definition of findHigh function goes here
+// This function accepts an array as an input argument, searches for the highest value in the array, and returns both the largest value and it's index position in the list
+template <typename A, typename B, typename C, typename D>
+void findHigh (A fpData [], B size, C & index, D & fpHigh) {
+    for (int i = 0; i < size; i++) {
+            if (fpData[i] > fpHigh) {
+                index = i;
+                fpHigh = fpData[i];
+            }
+
+        }
+
+    }
+
